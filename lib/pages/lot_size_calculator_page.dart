8558ec4d18196b4f483cb46214eb1ce8f044ca09
calpi-s <qoocalpis@yahoo.co.jp>
2023-09-21 23:55:09 +0900

    3Cell大枠完成

--- lib/pages/lot_size_calculator_page.dart
+++ lib/pages/lot_size_calculator_page.dart
@@ -20,7 +20,9 @@ class _LotSizeCalculatorPageState extends State<LotSizeCalculatorPage> {
         child: Stack(
           children: [
             _headSection(),
-            _listBills(),
+            accountBalanceCell(),
+            lossPercentCell(),
+            lossPipsCell(),
           ],
         ),
       ),
@@ -44,44 +46,36 @@ class _LotSizeCalculatorPageState extends State<LotSizeCalculatorPage> {
     );
   }
 
-  _listBills() {
+  accountBalanceCell() {
     double startPosition = MediaQuery.of(context).size.height * 0.22;
     double w = MediaQuery.of(context).size.width;
 
     return Positioned(
       top: startPosition,
       child: Container(
-        height: 100,
+        //height: 200,
         width: w * 0.9,
-        decoration: const BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-              topRight: Radius.circular(15.0),
-              bottomRight: Radius.circular(15.0),
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Color(0xFFd8dbe0),
-                offset: Offset(1, 1),
-                blurRadius: 20.0,
-                spreadRadius: 10,
-              )
-            ]),
+        decoration: _boxDecoration(),
         child: Container(
           margin: const EdgeInsets.all(15),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Container(
-                alignment: Alignment.topLeft,
-                child: _cellTitle(AppLocalizations.of(context)!.accountBalance),
-              ),
-              Container(
-                width: 100,
-                child: const TextField(
-                  textAlign: TextAlign.right,
-                  keyboardType: TextInputType.number,
-                ),
+              _cellTitle(AppLocalizations.of(context)!.accountBalance),
+              const Row(
+                children: <Widget>[
+                  SizedBox(
+                    width: 100,
+                  ),
+                  SizedBox(
+                    width: 170,
+                    child: TextField(
+                      textAlign: TextAlign.right,
+                      keyboardType: TextInputType.number,
+                    ),
+                  ),
+                  Text("USD"),
+                ],
               ),
             ],
           ),
@@ -91,13 +85,107 @@ class _LotSizeCalculatorPageState extends State<LotSizeCalculatorPage> {
     );
   }
 
-  _cellTitle(String cellTitle) {
-    return Text(
-      cellTitle,
-      style: const TextStyle(
-        color: Color.fromARGB(255, 49, 49, 49),
-        fontWeight: FontWeight.w700,
+  lossPercentCell() {
+    double startPosition = MediaQuery.of(context).size.height * 0.37;
+    double w = MediaQuery.of(context).size.width;
+
+    return Positioned(
+      top: startPosition,
+      child: Container(
+        width: w * 0.9,
+        decoration: _boxDecoration(),
+        child: Container(
+          margin: const EdgeInsets.all(15),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              _cellTitle(AppLocalizations.of(context)!.lossAllowance),
+              const Row(
+                children: [
+                  SizedBox(
+                    width: 100,
+                  ),
+                  SizedBox(
+                    width: 170,
+                    child: TextField(
+                      textAlign: TextAlign.right,
+                      keyboardType: TextInputType.number,
+                    ),
+                  ),
+                  Text("USD"),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
+    );
+  }
+
+  lossPipsCell() {
+    double startPosition = MediaQuery.of(context).size.height * 0.52;
+    double w = MediaQuery.of(context).size.width;
+    return Positioned(
+      top: startPosition,
+      child: Container(
+        width: w * 0.9,
+        decoration: _boxDecoration(),
+        child: Container(
+          margin: const EdgeInsets.all(15),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              _cellTitle(AppLocalizations.of(context)!.lossTolerancePips),
+              const Row(
+                children: [
+                  SizedBox(
+                    width: 100,
+                  ),
+                  SizedBox(
+                    width: 170,
+                    child: TextField(
+                      textAlign: TextAlign.right,
+                      keyboardType: TextInputType.number,
+                    ),
+                  ),
+                  Text("Pips"),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _cellTitle(String cellTitle) {
+    return Container(
+      alignment: Alignment.topLeft,
+      child: Text(
+        cellTitle,
+        style: const TextStyle(
+          color: Color.fromARGB(255, 49, 49, 49),
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+    );
+  }
+
+  _boxDecoration() {
+    return const BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.only(
+        topRight: Radius.circular(15.0),
+        bottomRight: Radius.circular(15.0),
+      ),
+      boxShadow: [
+        BoxShadow(
+          color: Color(0xFFd8dbe0),
+          offset: Offset(1, 1),
+          blurRadius: 20.0,
+          spreadRadius: 10,
+        ),
+      ],
     );
   }
 }
