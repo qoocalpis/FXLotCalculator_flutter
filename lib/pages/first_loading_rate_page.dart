1ffd7bb1e0ba3c5741197d809a60a9c85acde63c
川人悠生 <y.kawahito.ft@hitachi-kenki.com>
2023-10-04 23:53:54 +0900

    statelessに変更

--- lib/pages/first_loading_rate_page.dart
+++ lib/pages/first_loading_rate_page.dart
@@ -2,14 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:lot_size_calculator_app/component/colors.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class FirstLoadingRatePage extends StatefulWidget {
-  const FirstLoadingRatePage({Key? key}) : super(key: key);
+class FirstLoadingRatePage extends StatelessWidget {
+  const FirstLoadingRatePage({super.key});
 
-  @override
-  FirstLoadingRatePageState createState() => FirstLoadingRatePageState();
-}
-
-class FirstLoadingRatePageState extends State<FirstLoadingRatePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
