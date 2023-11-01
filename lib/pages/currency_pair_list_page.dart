c08b5ad25f094cdb5f4d28966b361e5acdabb55c
川人悠生 <y.kawahito.ft@hitachi-kenki.com>
2023-11-01 16:40:01 +0900

    currencyPairListの続き

--- lib/pages/currency_pair_list_page.dart
+++ lib/pages/currency_pair_list_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lot_size_calculator_app/models/currency_pair_object.dart';
 import 'package:lot_size_calculator_app/utils/colors.dart';
+import 'package:lot_size_calculator_app/utils/constants.dart';
 
 class CurrencyPairListPage extends ConsumerWidget {
   const CurrencyPairListPage({super.key});
@@ -26,12 +27,75 @@ class CurrencyPairListPage extends ConsumerWidget {
       body: ListView.builder(
         itemCount: CurrencyPairObject.currencyPairList.length, // この行を追加
         itemBuilder: (BuildContext context, int index) {
-          return Container(
-            height: 80,
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.yellow),
+          return GestureDetector(
+            child: Container(
+              height: 80,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: const Color.fromARGB(255, 140, 140, 138),
+                ),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      const Text(AppConst.strEmpty),
+                      Text(
+                        CurrencyPairObject.currencyPairList[index],
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                      const Text(
+                        "JapaneseYen vs US dollar",
+                        style: TextStyle(fontSize: 15),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        width: 40.0,
+                        height: 40.0,
+                        decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                                fit: BoxFit.fill,
+                                image: AssetImage("images/USD.png"))),
+                      ),
+                      const Text(
+                        "/",
+                        style: TextStyle(
+                          fontSize: 50,
+                          fontWeight: FontWeight.w100,
+                        ),
+                      ),
+                      Container(
+                        width: 40.0,
+                        height: 40.0,
+                        decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                                fit: BoxFit.fill,
+                                image: AssetImage("images/USD.png"))),
+                      ),
+                    ],
+                  ),
+                  const Icon(
+                    Icons.grade,
+                    size: 35,
+                    color: Colors.orange,
+                  ),
+                ],
+              ),
             ),
-            child: Text(CurrencyPairObject.currencyPairList[index]),
+            onTap: () {
+              print(CurrencyPairObject.currencyPairList[index]);
+            },
           );
         },
       ),
