366fab1da8e54f8a4d4e7510408723c0afa94bea
calpi-s <qoocalpis@yahoo.co.jp>
2024-01-13 22:25:17 +0900

    InAppPurchase4

--- lib/services/purchase_api.dart
+++ lib/services/purchase_api.dart
@@ -2,38 +2,8 @@ import 'dart:io' show Platform;
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:purchases_flutter/purchases_flutter.dart';
-
-Future<void> buttonAction() async {
-  const offeringIdentifier = "com.all_currency_pair.app";
-  try {
-    Offerings offerings = await Purchases.getOfferings();
-    final package = offerings.getOffering(offeringIdentifier)!.lifetime;
-    if (package != null) {
-      // Display packages for sale
-      await buyPackage(package);
-    }
-  } on PlatformException catch (e) {
-    // optional error handling
-  }
-}
-
-Future<void> buyPackage(Package package) async {
-  if (kDebugMode) {
-    print("InAppPurchase buyPackage");
-  }
-  try {
-    CustomerInfo customerInfo = await Purchases.purchasePackage(package);
-    var isPro =
-        customerInfo.entitlements.all["com.all_currency_pair.app"]!.isActive;
-    if (isPro) {
-      // Unlock that great "pro" content
-    }
-  } on PlatformException catch (e) {
-    var errorCode = PurchasesErrorHelper.getErrorCode(e);
-    if (errorCode != PurchasesErrorCode.purchaseCancelledError) {}
-  }
-}
 
 class PurchaseApi {
   /// private constructor
@@ -41,42 +11,109 @@ class PurchaseApi {
 
   /// the one and only instance of this singleton
   static final instance = PurchaseApi._();
+  bool isSubscribed = false;
+  late Offerings offerings;
 
-  Future<void> initPlatformState() async {
+  Future<void> initInAppPurchase() async {
     const iosKey = "appl_LCjrwQcWUMmKqjqvtvFPUzEbhpf";
     const androidKey = "goog_oKQsEOIJdGnMwLrtAOMQqkghyXF";
 
     await Purchases.setLogLevel(LogLevel.debug);
 
-    PurchasesConfiguration? configuration;
-    if (Platform.isAndroid) {
-      configuration = PurchasesConfiguration(androidKey);
-    }
-    if (Platform.isIOS) {
-      configuration = PurchasesConfiguration(iosKey);
-    }
-    if (configuration != null) {
+    try {
+      late PurchasesConfiguration configuration;
+      if (Platform.isAndroid) {
+        configuration = PurchasesConfiguration(androidKey);
+      }
+      if (Platform.isIOS) {
+        configuration = PurchasesConfiguration(iosKey);
+      }
       await Purchases.configure(configuration);
+      //offeringsを取ってくる
+      offerings = await Purchases.getOfferings();
+
+      //今アクティブになっているアイテムは以下のように取得可能
+      // print("アクティブなアイテム ${result.customerInfo.entitlements.active.keys}");
+    } catch (e) {
+      print("initInAppPurchase error caught! ${e.toString()}");
     }
   }
 
-  static Future<List<Offering>> fetchOffers() async {
+  Future<void> buttonAction() async {
+    const offeringIdentifier = "com.all_currency_pair.app";
     try {
-      final offerings = await Purchases.getOfferings();
-      final current = offerings.current;
-      return current == null ? [] : [current];
+      final package = offerings.getOffering(offeringIdentifier)!.lifetime;
+      if (package != null) {
+        // Display packages for sale
+      }
     } on PlatformException catch (e) {
       // optional error handling
-      return [];
     }
   }
+}
 
-  static Future<bool> purchasePackage(Package package) async {
-    try {
-      await Purchases.purchasePackage(package);
-      return true;
-    } catch (e) {
-      return false;
-    }
-  }
+final inAppPurchaseManager =
+    ChangeNotifierProvider((ref) => InAppPurchaseManager());
+
+class InAppPurchaseManager with ChangeNotifier {
+  bool isSubscribed = false;
+  late Offerings offerings;
+
+  // Future<void> initInAppPurchase() async {
+
+  //   try {
+  //     //consoleにdebug情報を出力する
+  //     await Purchases.setDebugLogsEnabled(true);
+  //     late PurchasesConfiguration configuration;
+
+  //     if (Platform.isAndroid) {
+  //       configuration = PurchasesConfiguration(Android用のRevenuecat APIキー);
+  //     } else if (Platform.isIOS) {
+  //       configuration = PurchasesConfiguration(ios用のRevenuecat APIキー);
+  //     }
+  //     await Purchases.configure(configuration);
+  //     //offeringsを取ってくる
+  //     offerings = await Purchases.getOfferings();
+  //     //firebaseのidと、revenuecatのuserIdを一緒にしている場合、firebaseAuthのuidでログイン
+  //     final result = await Purchases.logIn(auth.currentUser!.uid);
+
+  //     await getPurchaserInfo(result.customerInfo);
+
+  //     //今アクティブになっているアイテムは以下のように取得可能
+  //     print("アクティブなアイテム ${result.customerInfo.entitlements.active.keys}");
+  //   } catch (e) {
+  //     print("initInAppPurchase error caught! ${e.toString()}");
+  //   }
+
+  // }
+
+  // Future<void> getPurchaserInfo(
+  //     CustomerInfo customerInfo) async {
+
+  //   try {
+  //     isSubscribed = await updatePurchases(customerInfo, monthly_subscription);//monthly_subscriptionは、適宜ご自身のentitlement名に変えてください
+
+  //   } on PlatformException catch (e) {
+  //     print(" getPurchaserInfo error ${e.toString()}");
+  //   }
+  // }
+
+  // Future<bool> updatePurchases(
+  //     CustomerInfo purchaserInfo, String entitlement) async {
+  //   var isPurchased = false;
+  //   final entitlements = purchaserInfo.entitlements.all;
+  //   if (entitlements.isEmpty) {
+  //     isPurchased = false;
+  //   }
+  //   if (!entitlements.containsKey(entitlement)) {
+  //     ///そもそもentitlementが設定されて無い場合
+  //     isPurchased = false;
+  //   } else if (entitlements[entitlement]!.isActive) {
+  //     ///設定されていて、activeになっている場合
+  //     isPurchased = true;
+  //   } else {
+  //     isPurchased = false;
+  //   }
+  //   return isPurchased;
+  // }
 }
